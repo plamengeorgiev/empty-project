0cdeaff59fd32caeda01cc52b80854ccff4d355f
Plamen Georgiev <plamen.flame@gmail.com>
2020-08-07 14:49:27 +0300

    Updates

---
+++ precache-manifest.0e52c7fc64fafb335535614025850ac6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5b5bc74ba3f171acae9f9c86c54f9e",
+    "url": "/empty-project/index.html"
+  },
+  {
+    "revision": "f72d7b1cda87c99dbfc9",
+    "url": "/empty-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09472d862469dae36526",
+    "url": "/empty-project/static/js/2.59ce4b15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/empty-project/static/js/2.59ce4b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72d7b1cda87c99dbfc9",
+    "url": "/empty-project/static/js/main.1bb19881.chunk.js"
+  },
+  {
+    "revision": "a12f8a1ee7326357e6db",
+    "url": "/empty-project/static/js/runtime-main.9713de75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/empty-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
